5f713316471d2510e91bd8e9df635f179e0b4d20
Mohammad Aminsafaei <arc13.net@hotmail.com>
2022-04-27 11:15:52 +0430

    Add project files.

---
+++ EscPrinterTest/Printer/BitmapData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+namespace EscPrinterTest.Printer;
+
+public class BitmapData
+{
+    public BitArray Dots { get; set; }
+
+    public int Height { get; set; }
+
+    public int Width { get; set; }
+}
---
+++ EscPrinterTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using EscPrinterTest.Printer;
+using System.Reflection;
+
+const string ipAddress = "192.168.1.240";
+const int portNumber = 9100;
+
+EscPrinter printer = new EscPrinter(ipAddress, portNumber);
+
+string currentExecutionPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+string imagePath = Path.Combine(currentExecutionPath, "imagePng80q5x.Png");
+
+await printer.Print(imagePath);
+
+Console.WriteLine("Image printed successfully !!");
+Console.ReadKey();
